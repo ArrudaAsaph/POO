8e9bd8c6ec6e8a329c42bec40e956a0deab8cb8c
Asaph Arruda <asapharruda@gmail.com>
2024-10-28 12:21:18 -0300

    MUD: Adicionando Encapsulamento em Circulo.py

--- Python/Circulo.py
+++ Python/Circulo.py
@@ -1,24 +1,59 @@
 # 1. Um Círculo
-# A classe deve ter um atributo raio para armazenar a dimensão da figura e métodos para calcular sua área e sua
-# circunferência.
+# Escrever a classe Círculo de acordo com o diagrama UML apresentado abaixo. A classe deve ter um atributo raio
+# para armazenar a dimensão da figura e métodos para calcular sua área e sua circunferência, além dos métodos de
+# acesso para definir e recuperar o atributo raio.
 # Escrever um programa para testar a classe.
-class Circulo:
+
+class Circulo():
     def __init__(self):
         self.raio = 0
+    
+    def set_raio(self,v):
+        if (v >= 0): self.raio = v
+        else:
+            raise ValueError("a base do triângulo não pode ser negativa")
 
-    def calculaArea(self):
-        area =  (self.raio ** 2) * 3.14
-        return area
+    def get_raio(self):
+        return self.raio
 
-    def calculaCircunferencia(self):
-        circunferencia =( 3.14 * 2 )* self.raio
-        return circunferencia
-
+    def calcu_Area(self):
+        return self.raio ** 2 * 3.14
+    
+    def calcu_Circuferencia(self):
+        return 2 * 3.14 * self.raio
+    
 circulo = Circulo()
-circulo.raio = float(input("Digite o Raio do seu Círculo: "))
 
-area = circulo.calculaArea()
-circuferencia = circulo.calculaCircunferencia()
+circulo.set_raio(float(input("Digite o raio do círculo: ")))
 
-print(f"A area do seu círculo é: {area}")
-print(f"A circuferencia do seu círculo é: {circuferencia:.2f}")
+
+op = 0
+while op != 5:
+    print(""" 
+***************************************
+Digite a Operação que deseja realizar:
+1 - Calcular Área.
+2 - Calcular Circuferência.
+3 - Valor do Raio.
+4 - Novo valor do Raio.
+5 - Sair
+***************************************
+
+""")
+    op = int(input("Digite a Operação que deseja: "))
+    if (op == 1):
+        print(f"Área do Círculo = {circulo.calcu_Area():.2f}")
+    elif (op == 2):
+        print(f"Circuferência do Círculo = {circulo.calcu_Circuferencia():.2f}")
+    elif (op == 3):
+        print(f"Raio = {circulo.get_raio():.2f}")
+    elif (op == 4):
+        circulo.set_raio(float(input("Digite um novo raio do círculo: ")))
+    else:
+            op = int(input("Valor inválido! Digite a Operação válida: "))
+
+
+
+        
+
+
