4a01172938a2eb195ee91f2b225a097e5cd48759
ASAPH BONIFACIO GUIMARAES ARRUDA <20241014040010@ifrn.local>
2024-12-11 11:49:47 -0300

    ADD: class_Cliente

--- Comércio_Eletronico/models/class_Cliente.py
+++ Comércio_Eletronico/models/class_Cliente.py
@@ -5,15 +5,43 @@ class Cliente:
         self.__telefone = ""
         self.__email = ""
         self.__senha = ""
-    
+        self.set_id(id)
+        self.set_nome(nome)
+        self.set_telefone(telefone)
+        self.set_email(email)
+        self.set_senha(senha)
+
     def set_id(self, id):
-        if (type(id) == int): self.__id = id
-        else: raise ValueError("O id precisa ser um inteiro")
+        self.__id = id
     
     def set_nome(self,nome):
-        nome = nome.split()
-        upperName = " "
-        for name in nome:
-            name.capitalize()
-            name += 
-        self.__nome = 
+       self.__nome = nome
+
+    def set_telefone(self,telefone):
+        self.__telefone = telefone
+    
+    def set_email(self,email):
+        self.__email = email
+
+    def set_senha(self,senha):
+        self.__senha = senha
+    
+    def get_id(self):
+        return self.__id
+    
+    def get_nome(self):
+        return self.__nome
+    
+    def get_telefone(self):
+        return self.__telefone
+    
+    def get_email(self):
+        return self.__email
+    
+    def get_senha(self):
+        return self.__senha
+    
+    def __str__(self):
+        return f"id: {self.get_id()} - Nome: {self.get_nome()} - Telefone: {self.get_telefone()} - Email: {self.get_email()}"
+    
+    
---
+++ Comércio_Eletronico/teste.py
@@ -0,0 +1,14 @@
+# if ("@" in email): self.__email = email
+#         else:   raise ValueError("Email inválido")
+
+#  nome = nome.split()
+#         space = " "
+#         for name in nome:
+#             name = name.capitalize()          
+#             if name == nome[0].capitalize():
+#                 upperName = name
+#                 upperName += space
+#             else:               
+#                 upperName += space
+#                 upperName += name
+#         self.__nome = upperName
